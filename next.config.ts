004e54a8b1f344d121e92363d30b2743cce304ce
GuilhermeVRocha <guilhermevieirar14@gmail.com>
2026-06-22 16:03:33 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  distDir: 'dist',
+  basePath: '/guilhermevieira', // ← NOME DO SEU REPOSITÓRIO
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
